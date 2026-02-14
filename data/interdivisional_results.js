707bb88598325cab50ce23817ff57db954467a86
agustinperalta745-ai <agustinperalta745@gmail.com>
2026-02-14 15:36:20 -0300

    Update interdivisional_results.js

--- data/interdivisional_results.js
+++ data/interdivisional_results.js
@@ -10,9 +10,9 @@ const interdivisionalResults = {
     "octavos-8": { score: { home: 1, away: 2 }, pens: null, winner: "away" }
   },
   cuartos: {
-    "cuartos-1": { score: { home: 1, away: 3 }, pens: null, winner: "away" },
+    "cuartos-1": { score: null, pens: null, winner: null },
     "cuartos-2": { score: null, pens: null, winner: null },
-    "cuartos-3": { score: null, pens: null, winner: null },
+    "cuartos-3": { score: { home: 1, away: 3 } , pens: null, winner: "away" },
     "cuartos-4": { score: null, pens: null, winner: null }
   },
   semis: {
