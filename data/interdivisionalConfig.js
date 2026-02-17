d08449f27f16e98100448abd1e7ec814faf63758
agustinperalta745-ai <agustinperalta745@gmail.com>
2026-02-17 16:18:21 -0300

    Update interdivisionalConfig.js

--- data/interdivisionalConfig.js
+++ data/interdivisionalConfig.js
@@ -1,4 +1,4 @@
 // Configuración de fechas para la Copa Interdivisional
 window.interdivisionalConfig = {
-  endDate: "YYYY-MM-DDTHH:mm:ss" // <-- Acá yo voy a poner la fecha final real
+  endDate: "2026-02-20T23:00:00" // <-- Acá yo voy a poner la fecha final real
 };
